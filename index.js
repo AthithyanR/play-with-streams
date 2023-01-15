ee6176cd616b0f5fccf06e5b6122daa1db34c1bb
Athithyan Rengaswamy <atr@Athithyans-MBP.bbrouter>
2023-01-15 11:39:29 +0530

    c

---
+++ index.js
@@ -0,0 +1,77 @@
+import { once } from "node:events";
+import { createReadStream, createWriteStream } from "node:fs";
+import { Transform, pipeline, finished } from "node:stream";
+import { promisify } from "node:util";
+import { createBrotliCompress, createGzip } from 'node:zlib'
+const asyncPipeline = promisify(pipeline);
+const sleep = promisify(setTimeout)
+
+const rs = createReadStream('in.txt');
+const ws = createWriteStream('in.txt.br');
+// const rs = createReadStream('The.Incredible.Hulk.mp4');
+// const ws = createWriteStream('The.Incredible.Hulk.mp4.gz');
+
+await asyncPipeline(rs, createBrotliCompress(), ws)
+
+// async iterable is slower than pipe
+/*
+check1: 5ms
+check2: 3.973ms
+
+check1: 5.191ms
+check2: 3.63ms
+
+check1: 4.641ms
+check2: 3.799ms
+
+check1: 4.727ms
+check2: 3.933ms
+
+check1: 4.812ms
+check2: 3.634ms
+
+check1: 5.008ms
+check2: 3.809ms
+
+check1: 5.046ms
+check2: 3.452ms
+
+check1: 4.434ms
+check2: 3.546ms
+*/
+// {
+//   const rs = createReadStream("in.txt");
+//   const ws = createWriteStream("out.txt");
+
+//   console.time("check1");
+//   for await (let chunk of rs) {
+//     if (!ws.write(chunk)) {
+//       await once(ws, "drain");
+//     }
+//   }
+//   console.timeEnd("check1");
+// }
+// {
+//   const rs = createReadStream("in.txt");
+//   const ws = createWriteStream("out.txt");
+//   console.time("check2");
+//   await asyncPipeline(rs, ws);
+//   console.timeEnd("check2");
+// }
+
+// const transform = new Transform({
+//     transform(chunk, _encoding, cb) {
+//         const upped = chunk.toString().split(' ').map((e) => e.charAt(0).toUpperCase() + e.slice(1)).join(' ')
+//         cb(null, upped)
+//     }
+// })
+
+// await asyncPipeline(rs, transform, ws)
+
+// function* checkThis() {
+//     yield 'yoo\n'
+//     yield 'yooo\n'
+//     yield 'yoooo\n'
+// }
+
+// await asyncPipeline(Readable.from(checkThis()), ws)
